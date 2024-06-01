e468c2a97d5b29c60c8c711f2d7a9a8e9334fa33
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-06-01 17:17:46 +0200

    a good v1 of the project is working!

--- purrpalette/core.py
+++ purrpalette/core.py
@@ -5,36 +5,40 @@ class PurrPalette:
     
     def __init__(self, palettes_path='palettes.csv'):
         self.palettes = pd.read_csv(palettes_path)
-        self.palettes.set_index('id', inplace=True)
+        self.palettes.set_index('name', inplace=True)
 
-    def _get_palette(self, id):
-        if id == 'random':
+    def _get_palette(self, name):
+        if name == 'random':
             return self.palettes.sample(1).iloc[0]
-        if id not in self.palettes.index:
-            raise ValueError(f"Palette with id '{id}' not found. Please choose from\n{self.palettes.index.values}")
-        return self.palettes.loc[id]
+        if name not in self.palettes.index:
+            raise ValueError(f"Palette with name '{name}' not found. Please choose from\n{self.palettes.index.values}")
+        return self.palettes.loc[name]
 
-    def load_cmap(self, id='random', cmap_type='continuous'):
-        palette = self._get_palette(id)
-        hex_list = eval(palette['hex'])
+    def load_cmap(self, name='random', cmap_type='continuous'):
+        palette = self._get_palette(name)
+        hex_list = eval(palette['palette'])
 
         if cmap_type == 'continuous':
-            cmap = LinearSegmentedColormap.from_list(name=f'{id}', colors=hex_list)
+            cmap = LinearSegmentedColormap.from_list(name=f'{name}', colors=hex_list)
         elif cmap_type == 'qualitative':
-            cmap = ListedColormap(name=f'{id}', colors=hex_list)
+            cmap = ListedColormap(name=f'{name}', colors=hex_list)
         else:
             raise ValueError("cmap_type argument must be 'continuous' or 'qualitative'")
 
-        self.id = id
+        self.name = name
         self.cmap = cmap
         return cmap
+    
+    def source(self, name='random'):
+        palette = self._get_palette(name)
+        return palette['source']
 
-    def hex(self, id='random'):
-        palette = self._get_palette(id)
-        return eval(palette['hex'])
+    def hex(self, name='random'):
+        palette = self._get_palette(name)
+        return eval(palette['palette'])
 
-    def rgb(self, id='random'):
-        hex_list = self.hex(id)
+    def rgb(self, name='random'):
+        hex_list = self.hex(name)
         rgb_list = [tuple(int(hex.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)) for hex in hex_list]
         return rgb_list
 
