ea50f75c5c61dfac50177891a743a23db5bb01c3
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-06-01 16:58:25 +0200

    add new parser

---
+++ parsers/scrape_paletteer.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# load the HTML file (https://pmassicotte.github.io/paletteer_gallery/)
+with open('parsers/paletteer.html', 'r') as file:
+    soup = BeautifulSoup(file, 'html.parser')
+
+# initialize lists to store data and the soup
+names = []
+palettes = []
+sections = soup.find_all('section', class_='level3')
+
+# iterate over each section found
+for section in sections:
+    
+    # get the name of the palette
+    name_tags = section.find_all('center')
+    for name_tag in name_tags:
+        name = name_tag.text.strip()
+        names.append(name)
+
+    # get the hex values of the palette
+    palette_tags = section.find_all('hr')
+    for palette_tag in palette_tags:
+        all_spans = palette_tag.find_previous_sibling('p').find_all('span')
+        palette = [span.text for span in all_spans]
+        palettes.append(palette)
+
+# save palettes to csv
+df = pd.DataFrame({
+    "name": names,
+    "palette": palettes
+})
+df.to_csv("purrpalette/paletteer.csv", index=False)
+
+print(f'Paletteer parsing finished with: {len(palettes)} palettes found')
---
+++ purrpalette/core.py
@@ -0,0 +1,44 @@
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+import pandas as pd
+
+class PurrPalette:
+    
+    def __init__(self, palettes_path='palettes.csv'):
+        self.palettes = pd.read_csv(palettes_path)
+        self.palettes.set_index('id', inplace=True)
+
+    def _get_palette(self, id):
+        if id == 'random':
+            return self.palettes.sample(1).iloc[0]
+        if id not in self.palettes.index:
+            raise ValueError(f"Palette with id '{id}' not found. Please choose from\n{self.palettes.index.values}")
+        return self.palettes.loc[id]
+
+    def load_cmap(self, id='random', cmap_type='continuous'):
+        palette = self._get_palette(id)
+        hex_list = eval(palette['hex'])
+
+        if cmap_type == 'continuous':
+            cmap = LinearSegmentedColormap.from_list(name=f'{id}', colors=hex_list)
+        elif cmap_type == 'qualitative':
+            cmap = ListedColormap(name=f'{id}', colors=hex_list)
+        else:
+            raise ValueError("cmap_type argument must be 'continuous' or 'qualitative'")
+
+        self.id = id
+        self.cmap = cmap
+        return cmap
+
+    def hex(self, id='random'):
+        palette = self._get_palette(id)
+        return eval(palette['hex'])
+
+    def rgb(self, id='random'):
+        hex_list = self.hex(id)
+        rgb_list = [tuple(int(hex.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)) for hex in hex_list]
+        return rgb_list
+
+
+
+if __name__ == '__main__':
+    pass
