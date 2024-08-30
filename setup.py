5ff6232ac7b1efece8d05408deabded6c6da5002
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-08-30 16:17:19 +0200

    prepare for new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypalettes",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     description="A large collection of color maps for Python",
     long_description=open("README.md").read(),
